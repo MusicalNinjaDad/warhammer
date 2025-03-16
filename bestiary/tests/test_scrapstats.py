903796db88fb52bff5f940164847b5bc968501e9
Mike Foster <i.am.mike.foster@gmail.com>
2025-03-16 20:02:19 +0000

    parametrize test_get_stats

--- bestiary/tests/test_scrapstats.py
+++ bestiary/tests/test_scrapstats.py
@@ -20,6 +20,10 @@ def amoeba(requests_session: requests.Session) -> BeautifulSoup:
     localcopy = Path("tests/assets/amoeba.html").resolve()
     return get_and_parse(str(localcopy), uri_root="file://", session=requests_session)
 
+@pytest.fixture
+def soup(requests_session: requests.Session, request: pytest.FixtureRequest) -> BeautifulSoup:
+    return get_and_parse(str(request.param), uri_root="file://", session=requests_session)
+
 def test_get_statblocks(amoeba: BeautifulSoup):
     statblocks = get_statblocks(amoeba)
     assert len(statblocks) == 1
@@ -43,23 +47,35 @@ def test_parse_block(amoeba: BeautifulSoup):
         "Fel": 0,
     }
 
-def test_get_stats(amoeba: BeautifulSoup):
-    assert get_stats(amoeba) == {
-        "Basic Profile": {
-        "M": 4,
-        "WS": 33,
-        "BS": 0,
-        "S": 3,
-        "T": 5,
-        "W": 11,
-        "I": 30,
-        "A": 3,
-        "Dex": 0,
-        "Ld": 0,
-        "Int": 0,
-        "Cl": 0,
-        "WP": 0,
-        "Fel": 0,
-    },
-    }
+
+@pytest.mark.parametrize(
+    ["soup", "stats"],
+    [
+        pytest.param(
+            Path("tests/assets/amoeba.html").resolve(),
+            {
+                "Basic Profile": {
+                    "M": 4,
+                    "WS": 33,
+                    "BS": 0,
+                    "S": 3,
+                    "T": 5,
+                    "W": 11,
+                    "I": 30,
+                    "A": 3,
+                    "Dex": 0,
+                    "Ld": 0,
+                    "Int": 0,
+                    "Cl": 0,
+                    "WP": 0,
+                    "Fel": 0,
+                },
+            },
+            id="amoeba",
+        ),
+    ],
+    indirect=["soup"],
+)
+def test_get_stats(soup: BeautifulSoup, stats):
+    assert get_stats(soup) == stats
     
