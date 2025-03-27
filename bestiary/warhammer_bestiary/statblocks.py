0a3c8889ee4b5526896e5c0824dec40993dbcda5
Mike Foster <i.am.mike.foster@gmail.com>
2025-03-27 19:59:30 +0000

    improve readability of generate_class

--- bestiary/warhammer_bestiary/statblocks.py
+++ bestiary/warhammer_bestiary/statblocks.py
@@ -1,4 +1,8 @@
 """Warhammer Fantasy Roleplay 1st edition StatBlocks."""
+
+from functools import partial
+from textwrap import indent
+
 from ttrpg_dice import d, statblock
 
 
@@ -6,14 +10,14 @@ from ttrpg_dice import d, statblock
 class Warhammer:
     """WRFP1e StatBlock Base."""
 
-    M = d(20) # No tests, but > 20 not plausible
+    M = d(20)  # No tests, but > 20 not plausible
     WS = d(100)
     BS = d(100)
     S = d(10)
     T = d(10)
     W = d(100)
     I = d(100)  # noqa: E741
-    A = d(20) # No tests, but > 20 not plausible
+    A = d(20)  # No tests, but > 20 not plausible
     Dex = d(100)
     Ld = d(100)
     Int = d(100)
@@ -21,20 +25,21 @@ class Warhammer:
     WP = d(100)
     Fel = d(100)
 
+
 def generate_class(beast: str, statblocks: dict[str, dict[str, int | str]]) -> list[str]:
-    """Create a Warhammer StatBlock froma key, value pair of scraped results."""
-    INDENT = "    "  # noqa: N806
+    """Create a Warhammer StatBlock from a key, value pair of scraped results."""
+    indented = partial(indent, prefix="    ")
+
     def safe(s: str) -> str:
-        return s.replace(" ","_")
+        return s.replace(" ", "_")
+
+    def classdef(name: str, stats: dict):
+        return [f"class {safe(name)}(Warhammer):"] + [indented(f"{stat} = {val}") for stat, val in stats.items()] + [""]
+
     if len(statblocks) == 1:
-        return [f"class {safe(beast)}(Warhammer):"] + [
-            f"{INDENT}{stat} = {val}" for stat, val in statblocks["Basic Profile"].items()
-        ] + [""]
+        stats = next(iter(statblocks.values()))
+        return classdef(beast, stats)
+
     return [f"class {safe(beast)}:"] + [
-        line 
-        for name, stats in statblocks.items()
-        for line in (
-            [f"{INDENT}class {safe(name)}(Warhammer):"] +
-            [f"{INDENT}{INDENT}{stat} = {val}" for stat, val in stats.items()] +
-            [""]
-        )]
+        indented(line) for name, stats in statblocks.items() for line in classdef(name, stats)
+    ]
